dd55227debccfa78baf4018bee643abbe88682bc
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-07-31 12:34:26 -0500

    Refactored code and moved or deleted old files. All the cropping code is now housed in ./cropper/cropper.go

---
+++ cropper/cropper.go
@@ -0,0 +1,156 @@
+package cropper
+
+import (
+	"image"
+	"log"
+	"os"
+
+	"gocv.io/x/gocv"
+)
+
+type Cropper struct {
+	FileName string
+}
+
+func (ptr *Cropper) Crop(fileName string) {
+	// TODO - Add Arg for cropped size offset
+	// TODO - Add Arg for dst dir
+	ptr.FileName = fileName
+
+	// Load Scan Image
+	img := gocv.NewMat()
+	img = loadImg(fileName)
+
+	// Generate Grayscale Image from Loaded Image
+	imgGray := gocv.NewMat()
+	gocv.CvtColor(img, &imgGray, gocv.ColorBGRToGray)
+
+	// Generate Threshold Image from Grayscale Image
+	imgThresh := gocv.NewMat()
+	_ = gocv.Threshold(imgGray, &imgThresh, 245, 255, gocv.ThresholdToZeroInv)
+	imgGray.Close()
+
+	// Get Contours from Threshold Image
+	contours := getContours(&imgThresh)
+	imgThresh.Close()
+
+	// Find Photo Contours
+	photoIdx := findPhoto(&contours)
+
+	// Created a Rotated Rectangle
+	rect := getPhotoRect(&contours, photoIdx)
+
+	// Create a 2d Rotational Matix from Rotated Rectangle
+	rotMat := getRotationalMatrix(&img, &rect)
+
+	// Rotate Original Scanned Image with Rotational Matrix
+	rotImg := rotateImage(&img, &rotMat)
+
+	// Crop Scan with Rotated Rectangle
+	croppedImg := gocv.NewMat()
+	szOffset := 30
+	// You have to flip Height & Width or it won't crop right
+	sz := image.Pt(rect.Height-szOffset, rect.Width-szOffset)
+	gocv.GetRectSubPix(rotImg, sz, rect.Center, &croppedImg)
+
+	// Save Cropped Image to File
+	// TODO - parse new file name from user input fileName ie. img_CROPPED.JPG
+	saveImg("cropped.JPG", &croppedImg)
+
+	// Clean Up
+	contours.vectors.Close()
+	contours.hierarchy.Close()
+	rotImg.Close()
+	croppedImg.Close()
+}
+
+func loadImg(fileName string) gocv.Mat {
+	_, err := os.Stat(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	img := gocv.IMRead(fileName, gocv.IMReadColor)
+	return img
+}
+
+func saveImg(fileName string, img *gocv.Mat) {
+	gocv.IMWrite(fileName, *img)
+}
+
+type Contours struct {
+	vectors   gocv.PointsVector
+	hierarchy gocv.Mat
+}
+
+func getContours(img *gocv.Mat) Contours {
+	hierarchy := gocv.NewMat()
+
+	vectors := gocv.FindContoursWithParams(
+		*img,
+		&hierarchy,
+		gocv.RetrievalTree,
+		gocv.ChainApproxSimple,
+	)
+
+	contours := Contours{
+		vectors:   vectors,
+		hierarchy: hierarchy,
+	}
+
+	return contours
+}
+
+func findPhoto(c *Contours) int {
+	photos := []int{}
+
+	for i := 0; i < c.hierarchy.Cols(); i++ {
+		v := c.hierarchy.GetVeciAt(0, i)
+		// Hierarchy contains relation of each contour to one another
+		// [next, previous, children, parent]
+		_, _, child, parent := v[0], v[1], v[2], v[3]
+		contourArea := gocv.ContourArea(c.vectors.At(i))
+		if parent == -1 && child != -1 && contourArea > 200 {
+			photos = append(photos, i)
+		}
+	}
+
+	if len(photos) > 1 {
+		log.Fatal("Error: Found more than one photo in scan")
+	}
+	return photos[0]
+}
+
+func getPhotoRect(c *Contours, Idx int) gocv.RotatedRect {
+	rect := gocv.MinAreaRect(c.vectors.At(Idx))
+	return rect
+}
+
+func getRotationalMatrix(img *gocv.Mat, rect *gocv.RotatedRect) gocv.Mat {
+	center := rect.Center
+	var angle float64
+	H := img.Rows()
+	W := img.Cols()
+
+	if W > H {
+		angle = rect.Angle - 90
+	} else {
+		angle = (90 - rect.Angle) * -1
+	}
+	// sz := image.Pt(W, H)
+	M := gocv.GetRotationMatrix2D(center, angle, 1)
+
+	return M
+}
+
+func rotateImage(img *gocv.Mat, rotMat *gocv.Mat) gocv.Mat {
+	rotImg := gocv.NewMat()
+
+	H := img.Rows()
+	W := img.Cols()
+
+	size := image.Pt(W, H)
+
+	gocv.WarpAffine(*img, &rotImg, *rotMat, size)
+
+	return rotImg
+}
---
+++ old_files/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"os"
+	"scan-n-crop/cropper"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	// Run()
+	cropper := cropper.Cropper{}
+	fileName := "./imgs-test/corner.JPG"
+	cropper.Crop(fileName)
+}
+
+func Run() {
+	middle := "./imgs-test/middle.JPG"
+	mid_rot := "./imgs-test/middle_rotated.JPG"
+	crooked := "./imgs-test/crooked.JPG"
+	corner := "./imgs-test/corner.JPG"
+
+	img := LoadImg(middle)
+	output := CropImg(&img)
+	SaveImg("./cropped/middle_cropped.JPG", &output)
+
+	img = LoadImg(mid_rot)
+	output = CropImg(&img)
+	SaveImg("./cropped/middle_rotated_cropped.JPG", &output)
+
+	img = LoadImg(crooked)
+	output = CropImg(&img)
+	SaveImg("./cropped/crooked_cropped.JPG", &output)
+
+	img = LoadImg(corner)
+	output = CropImg(&img)
+	SaveImg("./cropped/corner_cropped.JPG", &output)
+
+	img.Close()
+	output.Close()
+}
+
+func LoadImg(fileName string) gocv.Mat {
+	_, err := os.Stat(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	img := gocv.IMRead(fileName, gocv.IMReadColor)
+	return img
+}
+
+func SaveImg(fileName string, img *gocv.Mat) {
+	gocv.IMWrite(fileName, *img)
+}
+
+func CropImg(img *gocv.Mat) gocv.Mat {
+	output := gocv.NewMat()
+	straight := gocv.NewMat()
+
+	contours := GetContours(img)
+	photoIdx := FindPhoto(&contours)
+	rect := GetPhotoRect(&contours, photoIdx[0])
+
+	contours.C.Close()
+	contours.H.Close()
+
+	straight = StraightenImg(img, &rect)
+
+	szOffset := 30
+	sz := image.Pt(rect.Height-szOffset, rect.Width-szOffset)
+	fmt.Println("sz: ", sz)
+	gocv.GetRectSubPix(straight, sz, rect.Center, &output)
+
+	straight.Close()
+
+	return output
+}
+
+func StraightenImg(img *gocv.Mat, rect *gocv.RotatedRect) gocv.Mat {
+	output := gocv.NewMat()
+	center := rect.Center
+	var angle float64
+	H := img.Rows()
+	W := img.Cols()
+
+	if W > H {
+		angle = rect.Angle - 90
+	} else {
+		angle = (90 - rect.Angle) * -1
+	}
+
+	fmt.Printf("H: %v, W: %v, C: %v, A: %v\n", H, W, center, angle)
+	sz := image.Pt(W, H)
+
+	M := gocv.GetRotationMatrix2D(center, angle, 1)
+
+	gocv.WarpAffine(*img, &output, M, sz)
+
+	return output
+}
+
+type Contours struct {
+	C gocv.PointsVector
+	H gocv.Mat
+}
+
+func GetContours(img *gocv.Mat) Contours {
+	C := Contours{}
+	C.C = gocv.NewPointsVector()
+	C.H = gocv.NewMat()
+
+	imgGray := gocv.NewMat()
+	thresh := gocv.NewMat()
+
+	gocv.CvtColor(*img, &imgGray, gocv.ColorBGRToGray)
+	// _ = gocv.Threshold(imgGray, &thresh, 127, 255, gocv.ThresholdBinary)
+	_ = gocv.Threshold(imgGray, &thresh, 245, 255, gocv.ThresholdToZeroInv)
+
+	C.C = gocv.FindContoursWithParams(
+		thresh,
+		&C.H,
+		gocv.RetrievalTree,
+		gocv.ChainApproxSimple,
+	)
+
+	imgGray.Close()
+	thresh.Close()
+
+	return C
+}
+
+func FindPhoto(C *Contours) []int {
+	photoIdx := []int{}
+
+	for i := 0; i < C.H.Cols(); i++ {
+		v := C.H.GetVeciAt(0, i)
+		_, _, c, p := v[0], v[1], v[2], v[3]
+		contourArea := gocv.ContourArea(C.C.At(i))
+		// if p == 0 && c != -1 && contourArea > 2500 {
+		if p == -1 && c != -1 && contourArea > 200 {
+			photoIdx = append(photoIdx, i)
+		}
+	}
+	fmt.Printf("len(photoIdx): %v\n", len(photoIdx))
+	return photoIdx
+}
+
+func GetPhotoRect(S *Contours, Idx int) gocv.RotatedRect {
+	rect := gocv.MinAreaRect(S.C.At(Idx))
+	return rect
+}
+
+func DrawBoundingBox(rect gocv.RotatedRect, img *gocv.Mat) {
+	red := color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	pts := rect.Points
+
+	gocv.Line(img, pts[0], pts[1], red, 10)
+	gocv.Line(img, pts[1], pts[2], red, 10)
+	gocv.Line(img, pts[2], pts[3], red, 10)
+	gocv.Line(img, pts[3], pts[0], red, 10)
+}
